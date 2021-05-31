2ec5896ae3cdac3ef4ab1ae1ddc9d4e30cfbfd44
Andrew Klychkov <aklychko@redhat.com>
2021-05-31 11:34:28 +0200

    Fix lint error

--- plugins/filter/molecule_core.py
+++ plugins/filter/molecule_core.py
@@ -87,7 +87,8 @@ class FilterModule(object):
         """Return implemented filters."""
         if MOLECULE_IMPORT_ERROR:
             raise_from(
-                AnsibleError('molecule python package must be installed to use this plugin'),
+                AnsibleError('molecule python package must be '
+                             'installed to use this plugin'),
                 MOLECULE_IMPORT_ERROR
             )
 
